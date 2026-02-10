dadff03b8731ac2ca41115cbb6d2971c63cec0a9
RuthlessG-CYBER <pandasoumya604@gmail.com>
2026-02-10 12:31:28 +0530

    Admin_Panel_Changes

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { toast } from 'sonner';
 
-export const BASE_URL = 'http://localhost:4040/api/v1';
+export const BASE_URL = 'https://lhoshop-backend.onrender.com/api/v1';
 
 const api = axios.create({
     baseURL: BASE_URL,
